e5d170879b813409808531ce28c8fc10df96a164
deger4beger <deger.begerrr@gmail.com>
2022-04-07 20:33:06 +0600

    sass added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,15 @@
+import "./index.scss";
 import React from 'react';
 
 function App() {
   return (
-    <div>
-      Hello
+  	<div className="app">
+	    <div>
+	      If you still don't have an account - Signup
+	    </div>
+	    <div>
+	      Если у вас еще нет аккаунта - Зарегистрироваться
+	    </div>
     </div>
   );
 }
