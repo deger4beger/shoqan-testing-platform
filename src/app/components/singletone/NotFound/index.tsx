063ae377607b5bd01a496a01bf9d3a6789eccc15
deger4beger <deger.begerrr@gmail.com>
2022-04-08 19:22:46 +0600

    latest

--- src/app/components/singletone/NotFound/index.tsx
+++ src/app/components/singletone/NotFound/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Pane, Heading } from "evergreen-ui"
 
 function NotFound() {
   return (
-  	<div>
-	    Страница не найдена
-    </div>
+  	<Pane
+  		width="100vw"
+  		height="100vh"
+  		display="flex"
+  		justifyContent="center"
+  		alignItems="center"
+  	>
+  		<Heading size={900}>
+  			Страница не найдена
+  		</Heading>
+  	</Pane>
   );
 }
 
--- src/app/components/templates/AuthTemplate/index.tsx
+++ src/app/components/templates/AuthTemplate/index.tsx
@@ -5,6 +5,7 @@ interface AuthTemplateProps {
 	children?: React.ReactNode
 	title?: string
 	btnContent?: string
+	btnLoading?: boolean
 	btnOnClick?: (e: React.MouseEvent<HTMLDivElement>) => void
 }
 
@@ -12,6 +13,7 @@ const AuthTemplate: React.FC<AuthTemplateProps> = ({
 	children,
 	title,
 	btnContent,
+	btnLoading,
 	btnOnClick
 }) => {
 	return (
@@ -23,11 +25,11 @@ const AuthTemplate: React.FC<AuthTemplateProps> = ({
 				alignItems="center"
 			>
 			<Pane
-					width="300px"
 					display="flex"
 					alignItems="center"
 					flexDirection="column"
 					backgroundColor="gray200"
+					padding={20}
 				>
 				<Pane borderBottom="1px solid black" marginBottom="30px">
 					<Heading size={800}>
