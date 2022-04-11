158ea199306b97be86614fbe84e3bc1940ef3163
deger4beger <deger.begerrr@gmail.com>
2022-04-11 23:16:09 +0600

    profile additional features

--- src/app/pages/user/Cabinet/index.tsx
+++ src/app/pages/user/Cabinet/index.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import {Button, Pane, SelectMenu, TextInputField} from "evergreen-ui";
+import React, {useState} from "react"
+import {Button, Pane, SelectMenu, SendMessageIcon, TextInputField, Heading, WarningSignIcon } from "evergreen-ui"
 
 const UserCabinet = () => {
 
@@ -11,65 +11,75 @@ const UserCabinet = () => {
   return (
     <>
       <Pane
-        width="100vw"
-        height="100vh"
+        width="60%"
+        paddingTop={100}
+        margin="auto"
         display="flex"
-        justifyContent="center"
-        alignItems="center"
         flexDirection="column"
       >
-        <TextInputField
-          label="ФИО"
-          placeholder="Шокоров Владислав Юрьевич"
-          width={300}
-          onChange={onFullNameChange} value={fullName}
-        />
+      	<Pane width={480}>
+	      	<Heading size={700}>
+	      		<WarningSignIcon color="warning" marginRight={16} />
+	      		Заполните анкету, чтобы продолжить дальше:
+	      	</Heading>
+	        <TextInputField
+	          label="ФИО"
+	          placeholder="Введите ФИО"
+	          width="100%"
+	          marginTop={20}
+	          onChange={onFullNameChange} value={fullName}
+	        />
 
-        <SelectMenu
-          title="Выберите ВУЗ"
-          hasTitle={false}
-          options={[
-            'Apple', 'Apricot',
-            'Banana', 'Cherry',
-            'Cucumber']
-            .map(
-              (label) => ({label, value: label})
-            )}
-        >
-          <Button width={300}>
-            {"Выберите ВУЗ . . ."}
-          </Button>
-        </SelectMenu>
-        <br/>
-        <SelectMenu
-          title="Выберите специальность"
-          hasTitle={false}
-          options={[
-            'Apple', 'Apricot',
-            'Banana', 'Cherry',
-            'Cucumber']
-            .map(
-              (label) => ({label, value: label})
-            )}
-        >
-          <Button width={300}>
-            {"Выберите специальность . . ."}
-          </Button>
-        </SelectMenu>
-        <br/>
-        <SelectMenu
-          title="Выберите курс"
-          hasFilter={false}
-          hasTitle={false}
-          options={['1','2','3','4']
-            .map(
-              (label) => ({label, value: label})
-            )}
-        >
-          <Button width={300}>
-            {"Выберите курс . . ."}
-          </Button>
-        </SelectMenu>
+	        <SelectMenu
+	          title="Выберите ВУЗ"
+	          hasTitle={false}
+	          options={[
+	            'Apple', 'Apricot',
+	            'Banana', 'Cherry',
+	            'Cucumber']
+	            .map(
+	              (label) => ({label, value: label})
+	            )}
+	        >
+	          <Button width="100%">
+	            Выберите ВУЗ . . .
+	          </Button>
+	        </SelectMenu>
+	        <br/>
+	        <SelectMenu
+	          title="Выберите специальность"
+	          hasTitle={false}
+	          options={[
+	            'Apple', 'Apricot',
+	            'Banana', 'Cherry',
+	            'Cucumber']
+	            .map(
+	              (label) => ({label, value: label})
+	            )}
+	        >
+	          <Button width="100%" marginTop={20}>
+	            Выберите специальность . . .
+	          </Button>
+	        </SelectMenu>
+	        <br/>
+	        <SelectMenu
+	          title="Выберите курс"
+	          hasFilter={false}
+	          hasTitle={false}
+	          options={['1','2','3','4']
+	            .map(
+	              (label) => ({label, value: label})
+	            )}
+	        >
+	          <Button width="100%" marginTop={20}>
+	            Выберите курс . . .
+	          </Button>
+	        </SelectMenu>
+	        <Button width="100%" marginTop={30} appearance="primary" intent="success">
+	        	<SendMessageIcon marginRight={16} />
+	        	Отправить данные
+	        </Button>
+        </Pane>
       </Pane>
     </>
   )
