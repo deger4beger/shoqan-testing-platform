60683c9d4efb7caf8c76f200993e232dc265c8c5
deger4beger <deger.begerrr@gmail.com>
2022-05-01 17:07:08 +0600

    some fixes

--- src/app/pages/user/MyStudy/Verification/index.tsx
+++ src/app/pages/user/MyStudy/Verification/index.tsx
@@ -17,12 +17,25 @@ const Verification = () => {
   const videoRef = useRef<null | HTMLVideoElement>(null)
   const photoRef = useRef<null | HTMLCanvasElement>(null)
 
+  const [faceSimilarity, setFaceSimilarity] = useState<null | number>(null)
   const [isPhotoDone, setIsPhotoDone] = useState(false)
+  const [isNetsLoading, setIsNetsLoading] = useState(false)
   const [isVerifying, setIsVerifying] = useState(false)
+  const [isVerificateDisabled, setIsVerificateDisabled] = useState(false)
+
 
   const { userStore } = useStores()
 
-  useEffect( ()=> {
+  useEffect( () => {
+    const loadNets = async () => {
+      setIsNetsLoading(true)
+      await faceapi.loadSsdMobilenetv1Model("/models")
+      await faceapi.loadTinyFaceDetectorModel("/models")
+      await faceapi.loadFaceLandmarkModel("/models")
+      await faceapi.loadFaceRecognitionModel("/models")
+      setIsNetsLoading(false)
+    }
+    loadNets()
     getVideo()
   }, [videoRef])
 
@@ -51,32 +64,41 @@ const Verification = () => {
     let ctx = photo!.getContext("2d")
     ctx!.drawImage(video as any, 0, 0, width, height)
     setIsPhotoDone(true)
+    isVerificateDisabled && setIsVerificateDisabled(false)
   }
 
   const verifyFace = async () => {
-    setIsVerifying(true)
-    await faceapi.loadSsdMobilenetv1Model("/models")
-    await faceapi.loadTinyFaceDetectorModel("/models")
-    await faceapi.loadFaceLandmarkModel("/models")
-    await faceapi.loadFaceRecognitionModel("/models")
+    try {
+      setIsVerifying(true)
 
-    const first = await faceapi
-      .detectSingleFace(photoRef.current!)
-      .withFaceLandmarks()
-      .withFaceDescriptor()
+      const first = await faceapi
+        .detectSingleFace(photoRef.current!)
+        .withFaceLandmarks()
+        .withFaceDescriptor()
 
-    const img = new Image()
-    img.crossOrigin = "anonymous"
-    img.src = userStore.profile!.photo
+      const img = new Image()
+      img.crossOrigin = "anonymous"
+      img.src = userStore.profile!.photo
 
-    const second = await faceapi
-      .detectSingleFace(img)
-      .withFaceLandmarks()
-      .withFaceDescriptor()
-    const result = await faceapi.euclideanDistance(first!.descriptor, second!.descriptor)
-    faceapi.draw.drawFaceLandmarks(photoRef.current!, first!)
-    console.log(result)
-    setIsVerifying(false)
+      const second = await faceapi
+        .detectSingleFace(img)
+        .withFaceLandmarks()
+        .withFaceDescriptor()
+
+      const result = await faceapi.euclideanDistance(first!.descriptor, second!.descriptor)
+      faceapi.draw.drawFaceLandmarks(photoRef.current!, first!)
+      setFaceSimilarity(Math.round((1 - result) * 100))
+      setIsVerifying(false)
+      setIsVerificateDisabled(true)
+    } catch (e) {
+      setFaceSimilarity(0)
+      setIsVerifying(false)
+      console.log(e)
+    }
+  }
+
+  const startTest = () => {
+    console.log("test started")
   }
 
   return (
@@ -92,7 +114,7 @@ const Verification = () => {
         <InfoSignIcon color="info" marginRight={16} />Пройдите верификацию для начала тестирования
       </Heading>
       <Heading size={200} marginTop={8}>
-        Сделайте фото профиля, схожешь должна быть &gt;80%
+        Сделайте фото профиля, схожешь должна быть &gt;50%
       </Heading>
     </Pane>
     <Pane display="flex" alignItems="center">
@@ -134,6 +156,7 @@ const Verification = () => {
           size="large"
           marginRight={20}
           disabled={isVerifying}
+          isLoading={isNetsLoading}
         >
         Сделать фото
       </Button>
@@ -144,12 +167,30 @@ const Verification = () => {
           size="large"
           intent="success"
           marginLeft={20}
-          disabled={!isPhotoDone}
+          disabled={!isPhotoDone || isVerificateDisabled}
           isLoading={isVerifying}
         >
         Верифицировать
       </Button>
     </Pane>
+    { faceSimilarity !== null && (
+      <Pane marginTop={40} borderBottom="2px solid #c1c4d6" paddingX={10} paddingBottom={2}>
+        <Strong size={500} color={faceSimilarity < 50 ? "red500" : "green600"}>
+          Схожесть - {faceSimilarity}%
+       </Strong>
+      </Pane>
+    ) }
+    { faceSimilarity !== null && <Button
+        onClick={startTest}
+        appearance="primary"
+        size="large"
+        intent="none"
+        marginTop={30}
+        disabled={!faceSimilarity || (faceSimilarity < 50)}
+        isLoading={false}
+      >
+      Начать тестирование
+    </Button> }
     </Pane>
   )
 }
